cd8a9ec02cd10b1815f947305768b39f16eb5a6c
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-07 16:01:46 +0000

    feature: Complete the logic for the carousel Automate sliding of images On clicking left, show left images On clicking right, show images on the right

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,62 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      carouselData: [],
+      currentImageUrl: '',
+      currentIndex: 0
     }
   }
   componentDidMount(){
+    this.setState({
+      carouselData: carouselData
+    })
+    this.autoSlideCarousel();    
+  }
 
+  autoSlideCarousel(){
+    setInterval(() => {
+      this.setState({
+        currentIndex: (this.state.currentIndex + 1) % this.state.carouselData.length
+      })
+    }, 5000)
   }
 
   leftClick = () => {
-
+    this.state.currentIndex === 0
+    ?
+      this.setState({
+        currentIndex: this.state.carouselData.length - 1
+      })
+    :
+      this.setState({
+        currentIndex: this.state.currentIndex - 1
+      })
   }
 
   rightClick = () => {
-
+    this.state.currentIndex < this.state.carouselData.length - 1
+    ?
+      this.setState({
+        currentIndex: this.state.currentIndex + 1
+      })
+    :
+      this.setState({
+        currentIndex: 0
+      })
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    let imgUrl = this.state.carouselData[this.state.currentIndex];
+    return <img src={imgUrl} alt="Carousel" style={{display: 'block'}} />
   }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        {
+          this.selectedImage()
+        }
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
