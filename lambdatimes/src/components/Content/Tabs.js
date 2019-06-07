11c316d9fb228994b8250284c78fb94980c11cc8
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-07 07:19:13 +0000

    feature: Map through tabs and render all tabs Destructure props from the top

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Tab from './Tab';
-const Tabs = props => {
+import '../../CSS/index.css';
+const Tabs = ({ tabs, selectTabHandler, selectedTab }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/
+            
+          tabs
+            .map((tab, index) => (
+              <Tab
+                key={index}
+                tab={tab}
+                selectTabHandler={selectTabHandler}
+                selectedTab={selectedTab}
+              />
+            )
+          )
+        }
       </div>
     </div>
   );
