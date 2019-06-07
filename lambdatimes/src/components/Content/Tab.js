9ac2255ae25dd1cfc09451409fa2e4f9c70bb567
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-07 07:22:01 +0000

    feature: Change tab classname based on selectedTab state Pass a function to change selected tab in state

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-const Tab = props => {
+const Tab = ({ tab, selectTabHandler, selectedTab }) => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
+      className={tab === selectedTab ? 'tab active-tab' : 'tab'}
+      onClick={(event) => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         selectTabHandler(event, tab)
       }}
     >
-      {props.tab.toUpperCase()}
+      {tab.toUpperCase()}
     </div>
   );
 };
